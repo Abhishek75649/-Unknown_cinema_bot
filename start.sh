250a7eaf9ac341a0af51733137e3d462b2041540
Abhixhek <121333829+mrabhixhek@users.noreply.github.com>
2024-03-25 23:07:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/abhithemachinegun/-Unknown_cinema_bot.git /-Unknown_cinema_bot 
+  git clone https://github.com/mrabhixhek/-Unknown_cinema_bot.git /-Unknown_cinema_bot 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /-Unknown_cinema_bot 
